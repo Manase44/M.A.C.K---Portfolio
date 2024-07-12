a17e002a943440ae42db2904aec78e0a02047ad8
tonie <anthophoenix24@gmail.com>
2024-07-12 19:58:43 +0300

    add work page

--- src/Pages/Work/Work.jsx
+++ src/Pages/Work/Work.jsx
@@ -1,74 +1,88 @@
 import React from "react";
 import "./work.css";
+import component from "../../assets/component.jpeg";
+import Malayams from "../../assets/malayam.jpeg";
+import delivery from "../../assets/delivery.jpeg";
+import potrait from "../../assets/potrait.jpeg";
 
 const Work = () => {
   return (
     <section>
-      <h1>Work</h1>
+      <h1 id="heading">Work</h1>
       <div className="workspace">
         <div className="workspace-div">
-          <div className="image"></div>
-          <div className="work-details">
-            <h2>Designing Dashboard</h2>
-            <section className="btn">
-              <button>2024</button>
-              <p>Dashboard</p>
-            </section>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga
-              harum magni quod quo consectetur vel, reprehenderit voluptatum
-              iste! A, aut!
-            </p>
-          </div>
-        </div>
+        <img src={component} alt="" className="image" />
 
-        <div className="workspace-div">
-          <div className="image"></div>
-          <div className="work-details">
-            <h2>Designing Dashboard</h2>
-            <section className="btn">
-              <button>2024</button>
-              <p>Dashboard</p>
-            </section>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga
-              harum magni quod quo consectetur vel, reprehenderit voluptatum
-              iste! A, aut!
-            </p>
-          </div>
-        </div>
-        <div className="workspace-div">
-          <div className="image"></div>
-          <div className="work-details">
-            <h2>Designing Dashboard</h2>
-            <section className="btn">
-              <button>2024</button>
-              <p>Dashboard</p>
-            </section>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga
-              harum magni quod quo consectetur vel, reprehenderit voluptatum
-              iste! A, aut!
-            </p>
-          </div>
-        </div>
-        <div className="workspace-div">
-          <div className="image"></div>
-          <div className="work-details">
-            <h2>Designing Dashboard</h2>
-            <section className="btn">
-              <button>2024</button>
-              <p>Dashboard</p>
-            </section>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga
-              harum magni quod quo consectetur vel, reprehenderit voluptatum
-              iste! A, aut!
-            </p>
-          </div>
+<div className="work-details">
+  <h2>Designing Dashboard</h2>
+  <section className="btn">
+    <button>2024</button>
+    <p id="heads">Dashboard</p>
+  </section>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
+    autem vitae deleniti reprehenderit facilis, quibusdam tenetur sit
+    possimus. Maxime ad beatae sed, nulla libero reiciendis dolores
+    tempore fugiat
+  </p>
+</div>
+       
+      
+      </div>
+      <hr />
+      <div className="workspace-div">
+        <img src={potrait} alt="" className="image" />
+        <div className="work-details">
+          <h2>Vibrant Potraits of 2020</h2>
+          <section className="btn">
+            <button>2024</button>
+            <p id="heads">Illustration</p>
+          </section>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
+            autem vitae deleniti reprehenderit facilis, quibusdam tenetur sit
+            possimus. Maxime ad beatae sed, nulla libero reiciendis dolores
+            tempore fugiat
+          </p>
         </div>
       </div>
+      <hr />
+      <div className="workspace-div">
+        <img src={Malayams} alt="" className="image" />
+        <div className="work-details">
+          <h2>36 Days of Malayams type</h2>
+          <section className="btn">
+            <button>2024</button>
+            <p id="heads">Typography</p>
+          </section>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
+            autem vitae deleniti reprehenderit facilis, quibusdam tenetur sit
+            possimus. Maxime ad beatae sed, nulla libero reiciendis dolores
+            tempore fugiat
+          </p>
+        </div>
+      </div>
+      <hr />
+      <div className="workspace-div">
+        <img src={delivery} alt="" className="image" />
+        <div className="work-details">
+          <h2>Components</h2>
+          <section className="btn">
+            <button>2024</button>
+            <p id="heads">Component Design</p>
+          </section>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
+            autem vitae deleniti reprehenderit facilis, quibusdam tenetur sit
+            possimus. Maxime ad beatae sed, nulla libero reiciendis dolores
+            tempore fugiat
+          </p>
+        </div>
+      </div>
+      </div>
     </section>
+   
   );
 };
 
