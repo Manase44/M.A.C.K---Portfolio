4d900a40e1142b03a4167095551efb0b76bfae6b
carolyne-wanjiru <shirogithenduka@gmail.com>
2024-07-12 15:51:42 +0300

    add the header and footer component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <section className="Header">
+      <div className="navigation">
+        <div className="navigation-list">
+          <div className="navigation-links">
+            <ol>
+              <li>Blog</li>
+              <li>Works</li>
+              <li>Contact</li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
