9c3912000929d6245c651db129be6574aafa17df
Manase44 <fikopersempre@gmail.com>
2024-07-16 17:10:02 +0300

    implemented the functionality of the hamburger menu and make it responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,21 @@
 import './Header.css';
 import { Link, NavLink } from "react-router-dom";
 import { RxHamburgerMenu } from "react-icons/rx";
+import HamburgerMenu from './HamburgerMenu';
+import { useState } from 'react';
 
 
 export const Header = () => {
+  const [openDialog, setOpenDiolog] = useState(false);
+
+  const toggleMenu = () => {
+    if (openDialog) {
+      setOpenDiolog(false)
+    }else{
+      setOpenDiolog(true)
+    }
+  }
+
   return (
     <header className="Header">
       <nav className="navigation">
@@ -19,7 +31,13 @@ export const Header = () => {
           </li>
         </ul>
       </nav>
-      <RxHamburgerMenu className='hamburger-menu'/>
+      <RxHamburgerMenu className='hamburger-menu' onClick={toggleMenu}/>
+     <div style={{backgroundColor: "red", width:"100vw", display: "flex", justifyContent:"end" }}>
+     <HamburgerMenu
+      open={openDialog}
+      close={() => {setOpenDiolog(false)}}
+      />
+     </div>
     </header>
   );
 }
