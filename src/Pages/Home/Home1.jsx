f21c41a05f2ef40a551d94e65c5f0cefd26e4107
Manase44 <fikopersempre@gmail.com>
2024-07-16 18:06:42 +0300

    implement the download resume functionality

--- src/Pages/Home/Home1.jsx
+++ src/Pages/Home/Home1.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import profile from "../../assets/image2.jpg";
 import "./home1.css";
+import resume from '../../assets/resume.pdf';
+import { Link } from "react-router-dom";
+
 const Home1 = () => {
   return (
     <section className="home-section">
@@ -19,7 +22,7 @@ const Home1 = () => {
               tempore fugiat
             </p>
           </div>
-          <button className="home-button">Download Resume</button>
+          <Link to={resume} target="_blank" className="home-button">Download Resume</Link>
         </div>
         <div className="image-side">
           <div className="home-image-holder">
